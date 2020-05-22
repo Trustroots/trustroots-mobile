f9ad74da77391fadbb7a38bbf0c4512e410fa7ab
Michael Straßburger <codepoet@cpan.org>
2020-05-22 04:15:06 +0200

    :sparkles: adding rn router flux, setting up basic router

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,16 @@
-import React from 'react';
-import { StyleSheet, View, Text, Image, useWindowDimensions } from 'react-native'
+import React from 'react'
+import { Router, Stack, Scene } from 'react-native-router-flux'
 
-import Logo from '../assets/logo-white.svg'
-import colors from './common/colors'
-import style from './common/styles'
-import Button from './components/Button';
+import Welcome from './scenes/Welcome'
 
 const App = () => {
-  const { width, height } = useWindowDimensions()
   return (
-    <>
-      <View style={styles.container}>
-        <Image
-          style={{width, height, position: 'absolute'}}
-          source={require('../assets/background.jpg')}
-          resizeMode='cover'
-        />
-
-        <Logo height={150} width={150} />
-
-        <Text style={styles.title}>
-          Travellers' community
-        </Text>
-        <Text style={styles.subtitle}>
-          Sharing, hosting and getting people together.
-        </Text>
-
-        <Button
-          label="Join Trustroots now"
-          width={200}
-          onPress={() => null}
-        />
-
-        <Button
-          label="Login"
-          width={200}
-          transparent
-          onPress={() => null}
-        />
-      </View>
-    </>
+    <Router>
+      <Stack hideNavBar key="root">
+        <Scene key="welcome" component={Welcome} title="Welcome" />
+      </Stack>
+    </Router>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  title: {
-    ...style.shadow,
-    textShadowRadius: 6,
-    marginTop: 20,
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: colors.foreground,
-    textAlign: 'center'
-  },
-  subtitle: {
-    ...style.shadow,
-    textShadowRadius: 4,
-    marginTop: 10,
-    fontWeight: 'bold',
-    color: colors.foreground,
-    textAlign: 'center'
-  }
-})
 
 export default App
---
+++ src/scenes/Welcome.tsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import { StyleSheet, View, Text, Image, useWindowDimensions } from 'react-native'
+
+import Logo from '../../assets/logo-white.svg'
+import colors from '../common/colors'
+import style from '../common/styles'
+import Button from '../components/Button'
+
+const Welcome = () => {
+  const { width, height } = useWindowDimensions()
+  return (
+    <View style={styles.container}>
+      <Image
+        style={{width, height, position: 'absolute'}}
+        source={require('../../assets/background.jpg')}
+        resizeMode='cover'
+      />
+
+      <Logo height={150} width={150} />
+
+      <Text style={styles.title}>
+        Travellers' community
+      </Text>
+      <Text style={styles.subtitle}>
+        Sharing, hosting and getting people together.
+      </Text>
+
+      <Button
+        label="Join Trustroots now"
+        width={200}
+        onPress={() => null}
+      />
+
+      <Button
+        label="Login"
+        width={200}
+        transparent
+        onPress={() => null}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    ...style.shadow,
+    textShadowRadius: 6,
+    marginTop: 40,
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: colors.foreground,
+    textAlign: 'center'
+  },
+  subtitle: {
+    ...style.shadow,
+    textShadowRadius: 4,
+    marginTop: 10,
+    marginBottom: 20,
+    fontWeight: 'bold',
+    color: colors.foreground,
+    textAlign: 'center'
+  }
+})
+
+export default Welcome
