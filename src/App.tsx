7d48b5317941dbf861dfb41bbb7ea904fdbef361
Michael Straßburger <codepoet@cpan.org>
2020-05-22 02:52:17 +0200

    :art: setting up SVG importer, tsconfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+
+import Logo from '../assets/logo-white.svg'
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Trustroots</Text>
+      <Logo height={100} width={100} />
     </View>
   );
 };
@@ -12,6 +14,7 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'darkgreen',
     alignItems: 'center',
     justifyContent: 'center'
   }
