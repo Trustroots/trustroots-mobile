3df156dd914d37d50ce37a6673f0ee26cffa3f51
Michael Straßburger <codepoet@cpan.org>
2020-05-22 19:04:43 +0200

    :art: adding reducer for app state

---
+++ src/reducers/app.ts
@@ -0,0 +1,53 @@
+import {
+  LOGIN_SUCCESS,
+  LOGOUT,
+  LOGIN_REQUEST,
+  LOGIN_ERROR,
+  REQUEST_ERROR
+} from '../common/constants'
+
+const initialState = {
+  session: null,
+  authenticating: null
+}
+
+export default function reducer(state = initialState, action: any = {}) {
+  const { type, payload } = action
+  switch (type) {
+    case REQUEST_ERROR:
+      return state.authenticating ? {
+        ...state,
+        authenticating: false
+      } : state
+
+    case LOGIN_REQUEST:
+      return {
+        ...state,
+        authenticating: true
+      }
+
+    case LOGIN_ERROR:
+      return {
+        ...state,
+        authenticating: false
+      }
+
+    case LOGIN_SUCCESS:
+      const { session } = payload
+      return {
+        ...state,
+        session,
+        authenticating: null
+      }
+
+    case LOGOUT:
+      return {
+        ...state,
+        session: null,
+        authenticating: null
+      }
+
+    default:
+      return state
+  }
+}
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux'
 import { createForms } from 'react-redux-form'
 
+import app from './app'
+
 export default combineReducers({
+  app,
   ...createForms({
     login: {},
     drafts: {},
